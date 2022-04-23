5ac3590967bc2afb352d368a5697dff5a1dd8c3b
totalnoMartina <totalnobzvz@yahoo.com>
2022-04-23 19:15:20 +0000

    Add models for category and product in product app

--- blackmoondesign/settings.py
+++ blackmoondesign/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     # 'allauth.socialaccount.providers.facebook',
     'mainapp',
+    'macrames',
 
 ]
 
--- macrames/models.py
+++ macrames/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Category(models.Model):
+    """ A category of the products with a user-friendly name """
+    name = models.CharField(max_length=250)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        """ A method to show name of the category """
+        return self.name
+
+    def get_friendly_name(self):
+        """ A method to show user-friendly name of the categories """
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """ A model of product attributes """
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    image = models.ImageField(null=False, blank=False)
+    name = models.CharField(max_length=200, null=False, blank=False)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField()
+    is_published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.name
