19065e836f917bc7d613ca5312bce453ce8ffd7c
totalnoMartina <totalnobzvz@yahoo.com>
2022-04-25 19:01:42 +0000

    Add logo, crispy forms, checkout template, order summary

--- blackmoondesign/settings.py
+++ blackmoondesign/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
 
+    # other apps
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +65,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'blackmoondesign.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -78,9 +82,14 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
 
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
