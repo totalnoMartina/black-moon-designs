a48a7dcf1be0201f14e0e5cc014369812a5ef560
totalnoMartina <totalnobzvz@yahoo.com>
2022-04-26 21:37:36 +0000

    Add js for the loader, try change the spinner

--- checkout/views.py
+++ checkout/views.py
@@ -67,7 +67,7 @@ def checkout(request):
         bag = request.session.get('bag', {})
         if not bag:
             messages.error(request, "There's nothing in your bag at the moment")
-            return redirect(reverse('products'))
+            return redirect(reverse('macrames'))
 
         current_bag = bag_contents(request)
         total = current_bag['grand_total']
