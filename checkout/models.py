9dc661842ec17804b0971ff74dcdf7787111dca0
totalnoMartina <totalnobzvz@yahoo.com>
2022-07-16 21:50:44 +0000

    Arrange order history and link to user profile, getting error on Attribute as 'None'

--- checkout/models.py
+++ checkout/models.py
@@ -6,13 +6,14 @@ from django.conf import settings
 
 from django_countries.fields import CountryField
 
-from profiles.models import UserProfile
 from macrames.models import Product
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
-    """ A class for oredr attributes """
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -26,6 +27,8 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
@@ -60,7 +63,6 @@ class Order(models.Model):
 
 
 class OrderLineItem(models.Model):
-    """ A model for a shopping bag (orderline)items attached to the order model """
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
