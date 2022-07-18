326d8f3c62ceb77434affa648acbe04e58598a28
totalnoMartina <totalnobzvz@yahoo.com>
2022-07-18 20:46:49 +0000

    Add different secret key, try test mode, webhooks almost work, add confirmation emails texts

--- blackmoondesign/settings.py
+++ blackmoondesign/settings.py
@@ -183,6 +183,8 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECREY = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'martinagoodliving"gmail.com'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,15 +1,14 @@
-import json
-import time
-
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
 
-from macrames.models import Product
-# from profiles.models import UserProfile
 from .models import Order, OrderLineItem
+from macrames.models import Product
+from profiles.models import UserProfile
 
+import json
+import time
 
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
@@ -22,18 +21,17 @@ class StripeWH_Handler:
         cust_email = order.email
         subject = render_to_string(
             'checkout/confirmation_emails/confirmation_email_subject.txt',
-
             {'order': order})
         body = render_to_string(
             'checkout/confirmation_emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
-
+        
         send_mail(
             subject,
             body,
             settings.DEFAULT_FROM_EMAIL,
             [cust_email]
-        )
+        )        
 
     def handle_event(self, event):
         """
@@ -78,13 +76,10 @@ class StripeWH_Handler:
 
         order_exists = False
         attempt = 1
-        # wile loop will make 5 attempts to check if order was already created
         while attempt <= 5:
             try:
-                # gets order details from the database
                 order = Order.objects.get(
                     full_name__iexact=shipping_details.name,
-                    # iexact ignores the uppercase
                     email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
                     country__iexact=shipping_details.address.country,
@@ -97,22 +92,17 @@ class StripeWH_Handler:
                     original_bag=bag,
                     stripe_pid=pid,
                 )
-                # breaks out of the loop if order exists
                 order_exists = True
                 break
             except Order.DoesNotExist:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
-            # by this time order has definitely been completed. webhook sends
-            # email confirmation to the user
             self._send_confirmation_email(order)
-            # if order exists returns 200 response
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200)
         else:
-            # creates a new order
             order = None
             try:
                 order = Order.objects.create(
@@ -149,16 +139,11 @@ class StripeWH_Handler:
                             order_line_item.save()
             except Exception as e:
                 if order:
-                    # deletes order if any errors occured and returns 500 error response
-                    # to stripe
                     order.delete()
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
-        # if the order has been created by the webhook handler
-        # the email is sent after the order was created
         self._send_confirmation_email(order)
-        # returns status 200 response
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
